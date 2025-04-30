a0998a4c47da430afb86ab6f97cb7c4cb84eaba0
said199 <61359272+said199@users.noreply.github.com>
2025-04-30 11:09:35 -0600

    Add fletgo-app/src/assets/flags/index.js 1746032975070

---
+++ fletgo-app/src/assets/flags/index.js
@@ -0,0 +1,8 @@
+export const flags = {
+  GT: 'https://flagcdn.com/w80/gt.png',
+  SV: 'https://flagcdn.com/w80/sv.png',
+  HN: 'https://flagcdn.com/w80/hn.png',
+  NI: 'https://flagcdn.com/w80/ni.png',
+  CR: 'https://flagcdn.com/w80/cr.png',
+  PA: 'https://flagcdn.com/w80/pa.png',
+};
