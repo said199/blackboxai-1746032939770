b79278dd67ea63ce603c35aca2c4be091963657d
said199 <61359272+said199@users.noreply.github.com>
2025-05-02 14:44:54 -0600

    Add fletgo-app/src/navigation/index.js 1746218694686

--- fletgo-app/src/navigation/index.js
+++ fletgo-app/src/navigation/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import DriverRegistrationScreen from '../screens/DriverRegistrationScreen';
+import DriverModeHome from '../screens/DriverModeHome';
 // importa otras pantallas necesarias
 
 const Stack = createStackNavigator();
@@ -12,6 +13,7 @@ export default function AppNavigator() {
       {/* otras pantallas */}
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="DriverRegistration" component={DriverRegistrationScreen} options={{ title: 'Registro de Conductor' }} />
+      <Stack.Screen name="DriverModeHome" component={DriverModeHome} options={{ title: 'Modo Conductor' }} />
     </Stack.Navigator>
   );
 }
